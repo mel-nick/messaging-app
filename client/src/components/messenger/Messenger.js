a7dff0136ce389c1e9c8d5c42cd07123273ef8b4
Nick Meleshchenko <nick.meleshchenko@gmail.com>
2021-09-26 11:01:05 +0300

    drawer behaviour fixed

--- client/src/components/messenger/Messenger.js
+++ client/src/components/messenger/Messenger.js
@@ -1,23 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect, useCallback, useContext } from "react";
-import { connect } from "react-redux";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Drawer from "@material-ui/core/Drawer";
-import Hidden from "@material-ui/core/Hidden";
-import { useTheme } from "@material-ui/core/styles";
-import axios from "axios";
-import Bar from "./Bar";
-import { useStyles } from "./messengerStyles";
-import SidePanel from "./SidePanel";
-import Footer from "./Footer";
-import Thread from "../messageThread/Thread";
+import React, { useState, useEffect, useCallback, useContext } from 'react';
+import { connect } from 'react-redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import { useTheme } from '@material-ui/core/styles';
+import axios from 'axios';
+import Bar from './Bar';
+import { useStyles } from './messengerStyles';
+import SidePanel from './SidePanel';
+import Footer from './Footer';
+import Thread from '../messageThread/Thread';
 import {
   setArrivalMessage,
   getChats,
   setCurrentUser,
-} from "../../actions/messaging";
-import { getOnlineUsers } from "../../actions/users";
-import { SocketContext } from "../../context";
+} from '../../actions/messaging';
+import { getOnlineUsers } from '../../actions/users';
+import { SocketContext } from '../../context';
 
 const Messenger = ({
   window,
@@ -36,7 +36,7 @@ const Messenger = ({
 
   const [mobileOpen, setMobileOpen] = useState(false);
   const [data, setData] = useState([]);
-  const [searchString, setSearchString] = useState("");
+  const [searchString, setSearchString] = useState('');
   const [typing, setTyping] = useState(false);
   const [incomingMessage, setIncomingMessage] = useState(null);
 
@@ -57,13 +57,13 @@ const Messenger = ({
   useEffect(() => {
     if (socket && loggedInUser) {
       socket.connect();
-      socket.emit("setUserOnline", loggedInUser);
-      socket.on("getOnlineUsers", (users) => getOnlineUsers(users));
+      socket.emit('setUserOnline', loggedInUser);
+      socket.on('getOnlineUsers', (users) => getOnlineUsers(users));
     }
 
     if (socket) {
       const onGetMessage = (message) => setIncomingMessage(message);
-      socket.on("getMessage", onGetMessage);
+      socket.on('getMessage', onGetMessage);
 
       if (
         currentUser &&
@@ -73,14 +73,14 @@ const Messenger = ({
         setArrivalMessage(incomingMessage);
       }
 
-      socket.once("isTyping", ({ from, to }) => {
+      socket.once('isTyping', ({ from, to }) => {
         loggedInUser?._id === to && currentUser?._id === from
           ? setTyping(true)
           : setTyping(false);
       });
 
       socket.once(
-        "notTyping",
+        'notTyping',
         ({ from, to }) =>
           loggedInUser?._id === to &&
           currentUser?._id === from &&
@@ -106,12 +106,12 @@ const Messenger = ({
     <div className={classes.root}>
       <CssBaseline />
       <Bar classes={classes} handleDrawerToggle={handleDrawerToggle} />
-      <nav className={classes.drawer} aria-label="mailbox folders">
-        <Hidden smUp implementation="css">
+      <nav className={classes.drawer} aria-label='mailbox folders'>
+        <Hidden smUp implementation='css'>
           <Drawer
             container={container}
-            variant="temporary"
-            anchor={theme.direction === "rtl" ? "right" : "left"}
+            variant='temporary'
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
             open={mobileOpen}
             onClose={handleDrawerToggle}
             classes={{
@@ -126,15 +126,16 @@ const Messenger = ({
               searchResults={data}
               setSearchData={setData}
               searchString={searchString}
+              handleDrawerToggle={handleDrawerToggle}
             />
           </Drawer>
         </Hidden>
-        <Hidden xsDown implementation="css">
+        <Hidden xsDown implementation='css'>
           <Drawer
             classes={{
               paper: classes.drawerPaper,
             }}
-            variant="permanent"
+            variant='permanent'
             open
           >
             <SidePanel
--- client/src/components/messenger/SidePanel.js
+++ client/src/components/messenger/SidePanel.js
@@ -1,19 +1,19 @@
-import React, { Fragment } from "react";
-import { useStyles } from "./messengerStyles";
-import { connect } from "react-redux";
-import Typography from "@material-ui/core/Typography";
-import Avatar from "@material-ui/core/Avatar";
-import LogOutSwitch from "./LogOutSwitch";
-import Divider from "@material-ui/core/Divider";
-import InputBase from "@material-ui/core/InputBase";
-import SearchIcon from "@material-ui/icons/Search";
-import ClearIcon from "@material-ui/icons/Clear";
-import User from "../user/User";
+import React, { Fragment } from 'react';
+import { useStyles } from './messengerStyles';
+import { connect } from 'react-redux';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import LogOutSwitch from './LogOutSwitch';
+import Divider from '@material-ui/core/Divider';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import ClearIcon from '@material-ui/icons/Clear';
+import User from '../user/User';
 import {
   setCurrentUser,
   getMessages,
   addOnlineUserToActiveChats,
-} from "../../actions/messaging";
+} from '../../actions/messaging';
 
 const SidePanel = ({
   loggedUser,
@@ -26,12 +26,13 @@ const SidePanel = ({
   currentUser,
   onlineUsers,
   activeChats,
+  handleDrawerToggle,
 }) => {
   const classes = useStyles();
 
   const handleSearchClear = () => {
     setSearchData([]);
-    setSearchString("");
+    setSearchString('');
   };
 
   const isUserOnline = (user, usersOnline) => {
@@ -51,8 +52,8 @@ const SidePanel = ({
           />
         </div>
         <div className={classes.userData}>
-          <Typography align="center" variant="h6">
-            {loggedUser?.firstName} {""} {loggedUser?.lastName}
+          <Typography align='center' variant='h6'>
+            {loggedUser?.firstName} {''} {loggedUser?.lastName}
           </Typography>
         </div>
         <div className={classes.search}>
@@ -64,12 +65,12 @@ const SidePanel = ({
             )}
           </div>
           <InputBase
-            placeholder="Search contacts…"
+            placeholder='Search contacts…'
             classes={{
               root: classes.inputRoot,
               input: classes.inputInput,
             }}
-            inputProps={{ "aria-label": "search" }}
+            inputProps={{ 'aria-label': 'search' }}
             value={searchString}
             onChange={(e) => {
               !e.target.value && handleSearchClear();
@@ -92,6 +93,7 @@ const SidePanel = ({
                   }
                   addOnlineUserToActiveChats(user);
                   handleSearchClear();
+                  !!handleDrawerToggle && handleDrawerToggle(false);
                 }}
               />
             ))
@@ -108,6 +110,7 @@ const SidePanel = ({
                     }
                     addOnlineUserToActiveChats(user);
                     handleSearchClear();
+                    !!handleDrawerToggle && handleDrawerToggle(false);
                   }}
                 />
               ))}
